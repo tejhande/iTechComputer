17b63a8992e7c83c47fa491a1c652d4b5f7e2918
tejhande <tejashande143143@gmail.com>
2025-10-31 11:57:23 +0530

    saas

--- MERN/product-management-app/backend/server.js
+++ MERN/product-management-app/backend/server.js
@@ -35,8 +35,9 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
